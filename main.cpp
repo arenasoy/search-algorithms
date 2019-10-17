bc8d6a7e028fe0ba40004e7bb22701c75a6426db
arenasoy <caro.arenas2607@gmail.com>
2019-10-16 22:29:33 -0300

    add dfs

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+pair<int, int> directions[] = {
+	make_pair(-1, -1), make_pair(0, -1), make_pair(1, -1),
+	make_pair(-1, 0), make_pair(1, 0),
+	make_pair(-1, 1), make_pair(0, 1), make_pair(1, 1)};
+
+bool in_limits(int n, int m, int i, int j) {
+	if (i < 0 || j < 0 || i >= n || j >= m) return false;
+	return true;
+}
+
 void print_board(vector<vector<char>> board) {
 	for (int i = 0; i < board.size(); i++) {
 		for (int j = 0; j < board[i].size(); j++) {
@@ -19,28 +29,52 @@ vector<vector<char>> write_result(vector<vector<char>> board, vector<pair<int, i
 	return board;
 }
 
-vector<pair<int, int>> dfs(vector<vector<char>> board, int x, int y) {
+vector<pair<int, int>> dfs_recursive(vector<vector<char>> board, bool **visited, int y, int x) {
 
-	
+	vector<pair<int, int>> v;
+	visited[y][x] = true;
+
+	for (int i = 0; i < 8; i++) {
+		int new_i = y + directions[i].first;
+		int new_j = x + directions[i].second;
+
+		if (in_limits(board.size(), board[0].size(), new_i, new_j)
+			&& !visited[new_i][new_j]) {
+			if (board[new_i][new_j] == '*') {
+				dfs_recursive(board, visited, new_i, new_j);
+			} else if (board[new_i][new_j] == '$') {
+				cout << "end at: " << new_i << ", " << new_j << endl;
+				return v;
+			}
+		}
+	}
+
+	return v;
 }
 
-bool in_limits(int n, int m, int i, int j) {
-	if (i < 0 || j < 0 || i >= n || j >= m) return false;
-	return true;
+vector<pair<int, int>> dfs(vector<vector<char>> board, int x, int y) {
+
+	bool **visited = (bool **) malloc(sizeof(bool*) * board.size());
+	for (int i = 0; i < board.size(); i++) {
+		visited[i] = (bool *) malloc(sizeof(bool) * board[0].size());
+		for (int j = 0; j < board[0].size(); j++) {
+			visited[i][j] = false;
+		}
+	}
+
+	bool finished = false;
+
+	return dfs_recursive(board, visited, y, x);
 }
 
 vector<pair<int, int>> bfs(vector<vector<char>> board, int x, int y) {
 
-	pair<int, int> directions[] = {
-		make_pair(-1, -1), make_pair(0, -1), make_pair(1, -1),
-		make_pair(-1, 0), make_pair(1, 0),
-		make_pair(-1, 1), make_pair(0, 1), make_pair(1, 1)};
-
+	vector<pair<int, int>> v;
 	bool visited[board.size()][board[0].size()];
 	bool finished = false;
 
 	for (int i = 0; i < board.size(); i++) {
-		for (int j = 0; j < board.size(); j++) {
+		for (int j = 0; j < board[0].size(); j++) {
 			visited[i][j] = false;
 		}
 	}
@@ -73,6 +107,8 @@ vector<pair<int, int>> bfs(vector<vector<char>> board, int x, int y) {
 
 	}
 
+	return v;
+
 }
 
 
@@ -80,7 +116,7 @@ vector<pair<int, int>> best_first(vector<vector<char>> board, int x, int y) {
 
 }
 
-vector<pair<int, int>> a_start(vector<vector<char>> board, int x, int y) {
+vector<pair<int, int>> a_star(vector<vector<char>> board, int x, int y) {
 
 }
 
@@ -112,23 +148,15 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 
-	// cout << "Busca em profundidade" << endl;
-	// dfs(board, x, y);
+	cout << "Busca em profundidade" << endl;
+	dfs(board, x, y);
 	cout << "Busca em largura" << endl;
 	bfs(board, x, y);
 	// cout << "Busca Best-first Search" << endl;
 	// best_first(board, x, y);
 	// cout << "Busca A*" << endl;
-	// a_start(board, x, y);
+	// a_star(board, x, y);
 
-	// vector<pair<int, int>> result;
-	// result.push_back(make_pair(0, 0));
-	// result.push_back(make_pair(1, 1));
-	// result.push_back(make_pair(2, 2));
-	// result.push_back(make_pair(3, 3));
-
-	// vector<vector<char>> res = write_result(board, result);
-	// print_board(res);
 
 	return 0;
 }
